14319466392b6f82dc9ae68f4bfbe9cdcd40c187
yashvi2504 <178709310+yashvi2504@users.noreply.github.com>
2025-11-21 22:47:11 +0530

    Fixed JSF MIME issues + added EncodingFilter

---
+++ src/main/java/filter/EncodingFilter.java
@@ -0,0 +1,17 @@
+package filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+}
